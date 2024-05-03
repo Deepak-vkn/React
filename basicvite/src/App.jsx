1297e9f9095731a2e561a185478e32c7108f3fba
deepak <deepakvkn1252@gmail.com>
2024-05-03 18:48:52 +0530

    Introduction to click event

--- basicvite/src/App.jsx
+++ basicvite/src/App.jsx
@@ -1,17 +1,9 @@
-import Fruit from "./Fruit"
+import Button from "./Button.jsx"
 
 function App() {
-  const fruit=[{name:'orange',calory:10},{name:'apple',calory:13},{name:'banana',calory:1},{name:'pineaple',calory:8}]
-
-    const veg=[{name:'pappaya',calory:10},{name:'tomato',calory:13},{name:'chilly',calory:1},{name:'potato',calory:8}]
-
-
- return(
-  <> <Fruit  items={fruit} catagory='fruits'/>
-  <Fruit  items={veg} catagory='vegitables'/>
-</>
- 
-)
+     return(
+      <Button></Button>
+     )
 }
 
 export default App
