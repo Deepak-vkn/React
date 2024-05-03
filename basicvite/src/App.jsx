3691e577f262dfa148cdeb49d48f56b8d471b591
deepak <deepakvkn1252@gmail.com>
2024-05-03 13:09:39 +0530

    Sample work out with heade and footer

---
+++ basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./header.jsx"
+import Footer from "./Footer.jsx"
+
+function App() {
+
+ return(
+  <> <Header></Header>
+  <Footer></Footer></>
+
+)
+  
+}
+
+export default App
---
+++ basicvite/src/header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+
+
+    return(
+
+        <header>
+            <h1>this is my header</h1>
+            <nav>and this is my nav</nav>
+            <ul>
+                <li>li1</li>
+                <li>li2</li>
+                <li>li3</li>
+            </ul>
+        </header>
+        
+    );
+}
+
+
+export default Header
